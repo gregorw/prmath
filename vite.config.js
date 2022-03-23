5fffd2328c01125686875fa00fd055e91bd1de5a
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-03-23 09:54:33 +0100

    Fix output dir

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
-  publicDir: "../dist",
+  publicDir: "../docs",
   plugins: [svelte()],
   server: {
     port: 5000
   },
   build: {
-    outDir: '../dist',
+    outDir: '../docs',
   },
   optimizeDeps: {
     include: ['@carbon/charts']
